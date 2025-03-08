4d91ec8674e951f982c7266de9d723230d74508b
PERUZZI GIOVANNI <12999427@itisrossi.edu.it>
2025-03-08 09:44:45 +0100

    cambio gioco

--- Cubetti/Form1.cs
+++ Cubetti/Form1.cs
@@ -8,39 +8,52 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Cubetti //TOPO: TESTARE SU MURI DI DIMENSIONI DIVERSE E USARE LE STRUCT E REF MEGLIO
+namespace Cubetti //TODO: USARE LE STRUCT E REF MEGLIO (?)
 {
+    /*
+    struct Punto //equivalente a Point ma uso Point perchè Location è un Point
+    {
+        public int X; public int Y;
+    }
+    */
+
     public partial class Form1 : Form
     {
-        Panel[] ostacoli;
-        int punteggio = 0;
+        Panel[] oggetti;
+        int score_puffo = 0;
+        int score_gargamella = 0;
+        Random rand = new Random(Environment.TickCount);
         public Form1()
         {
             InitializeComponent();
-            ostacoli = new Panel[1] { pnl_wall };
+            oggetti = new Panel[] { pnl_cassetta, pnl_gargamella, pnl_player }; //tutti gli oggetti (le collsioni le filtro dopo in base a .Tag)
         }
 
-        private void aggiornaPunteggio(int delta)
+        private void aggiornaPunteggio(int delta, Panel target)
         {
-            punteggio += delta;
-            lbl_score.Text = "PUNTEGGIO: " + punteggio;
-        }
-
-        private void sposta(int dx, int dy)
-        {
-            if (dx != 0) {
-                pnl_player.Location = new Point(spostaX(dx), pnl_player.Location.Y);
-            } else {
-                pnl_player.Location = new Point(pnl_player.Location.X, spostaY(dy));
+            if (target == pnl_player)
+            {
+                score_puffo += delta;
+                lbl_score_puffo.Text = "PUNTEGGIO: " + score_puffo;
+            }
+            else if (target == pnl_gargamella)
+            {
+                score_gargamella += delta;
+                lbl_score_gargamella.Text = "PUNTEGGIO: " + score_gargamella;
             }
         }
-        private int spostaX(int dx)
-        {
-            int x = pnl_player.Location.X;
-            int y = pnl_player.Location.Y;
-            int pos = Math.Max(0, Math.Min(pnl_game.Width - pnl_player.Width, x + dx));
 
-            foreach (Panel ostacolo in ostacoli)
+        private void sposta(int dx, int dy, Panel target)
+        {
+            target.Location = new Point(spostaX(dx, target), spostaY(dy, target));
+        }
+        private int spostaX(int dx, Panel target)
+        {
+            int x = target.Location.X;
+            int y = target.Location.Y;
+            int pos = Math.Max(0, Math.Min(pnl_game.Width - target.Width, x + dx));
+
+            foreach (Panel oggetto in oggetti)
             {
                 //ALTRO METODO CON RECTANGLE E LA SUA FUNZIONE INTERSECTSWITH
                 /*
@@ -54,59 +67,105 @@ namespace Cubetti //TOPO: TESTARE SU MURI DI DIMENSIONI DIVERSE E USARE LE STRUC
                 }
                 */
 
-                if ((y + pnl_player.Height > ostacolo.Location.Y) && (y < ostacolo.Location.Y + ostacolo.Height))
+                if ((y + target.Height > oggetto.Location.Y) && (y < oggetto.Location.Y + oggetto.Height) && oggetto != target) //non potevo fare una funzione appostita x questo perchè il codice è diverso in quanto valuto anche le estensioni degli oggetti
                 {
-                    if ((pos + pnl_player.Width > ostacolo.Location.X) && (pos < ostacolo.Location.X + ostacolo.Width))
+                    if ((pos + target.Width > oggetto.Location.X) && (pos < oggetto.Location.X + oggetto.Width))
                     {
-                        aggiornaPunteggio(-1);
-                        return x;
+                        if (handleCollision(target, oggetto)) { return x; };
+                        
                     }
                 }
             }
-            aggiornaPunteggio(+1);
             return pos;
         }
 
-        private int spostaY(int dy)
+        private int spostaY(int dy, Panel target)
         {
-            int x = pnl_player.Location.X;
-            int y = pnl_player.Location.Y;
-            int pos = Math.Max(0, Math.Min(pnl_game.Height - pnl_player.Height, y + dy));
+            int x = target.Location.X;
+            int y = target.Location.Y;
+            int pos = Math.Max(0, Math.Min(pnl_game.Height - target.Height, y + dy));
 
-            foreach (Panel ostacolo in ostacoli)
+            foreach (Panel oggetto in oggetti)
             {
-                if ((x + pnl_player.Width > ostacolo.Location.X) && (x < ostacolo.Location.X + ostacolo.Width))
+                if ((x + target.Width > oggetto.Location.X) && (x < oggetto.Location.X + oggetto.Width) && oggetto != target)
                 {
-                    if ((pos + pnl_player.Height > ostacolo.Location.Y) && (pos < ostacolo.Location.Y + ostacolo.Height))
+                    if ((pos + target.Height > oggetto.Location.Y) && (pos < oggetto.Location.Y + oggetto.Height))
                     {
-                        aggiornaPunteggio(-1);
-                        return y;
+                        if (handleCollision(target, oggetto)) { return y; };
                     }
                 }
             }
-            aggiornaPunteggio(+1);
             return pos;
         }
+
+        private bool handleCollision (Panel target, Panel collision)
+        {
+            if ((string)collision.Tag == "CASSETTA" && target == pnl_player)
+            {
+                spostaAlberi();
+                generaPosizioneValida(collision);
+                aggiornaPunteggio(+1, pnl_player);
+                return false;
+            }
+            else if (target == pnl_gargamella && collision == pnl_player)
+            {
+                generaPosizioneValida(pnl_player);
+                aggiornaPunteggio(+1, pnl_gargamella);
+            }
+
+            return true;
+        }
+
+        private void generaPosizioneValida (Panel obj)
+        {
+            //vericare se la location è valida (alberi)
+            Point punto = new Point((int)(rand.NextDouble() * (pnl_game.Width - obj.Width)), (int)(rand.NextDouble() * (pnl_game.Height - obj.Height)));
+
+            obj.Location = punto;
+        }
+
+        #region RiceviClick
 
         private void btn_left_Click(object sender, EventArgs e)
         {
-            sposta(-25, 0);
+            sposta(-25, 0, pnl_player);
         }
 
         private void btn_down_Click(object sender, EventArgs e)
         {
-            sposta(0, 25);
+            sposta(0, 25, pnl_player);
         }
 
         private void btn_up_Click(object sender, EventArgs e)
         {
-            sposta(0, -25);
+            sposta(0, -25, pnl_player);
         }
 
         private void btn_right_Click(object sender, EventArgs e)
         {
-            sposta(25, 0);
+            sposta(25, 0, pnl_player);
         }
 
+        private void btn_up_gargamella_Click(object sender, EventArgs e)
+        {
+            sposta(0, -18, pnl_gargamella);
+        }
+
+        private void btn_left_gargamella_Click(object sender, EventArgs e)
+        {
+            sposta(-18, 0, pnl_gargamella);
+        }
+
+        private void bhtn_right_gargamella_Click(object sender, EventArgs e)
+        {
+            sposta(18, 0, pnl_gargamella);
+        }
+
+        private void btn_down_gargamella_Click(object sender, EventArgs e)
+        {
+            sposta(0, 18, pnl_gargamella);
+        }
+
+        #endregion
     }
 }
